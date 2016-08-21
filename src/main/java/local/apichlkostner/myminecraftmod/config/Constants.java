23be4534b3781f288fcf9a3e0459545506228571
Arthur Pichlkostner <apichlkostner@gmx.de>
2016-08-21 13:12:11 +0200

    Improvement of steel armor stats

--- src/main/java/local/apichlkostner/myminecraftmod/config/Constants.java
+++ src/main/java/local/apichlkostner/myminecraftmod/config/Constants.java
@@ -4,15 +4,15 @@ import net.minecraft.block.material.Material;
 
 public class Constants {
 	public static final String MODID = "mymod";
-	public static final String MODNAME = "My own test mod";
+	public static final String MODNAME = "Test mod with steel items";
 	public static final String VERSION = "1.0";
 	
 	public static final String MOD_PATH = "local.apichlkostner.myminecraftmod";
 	public static final String CLIENTSIDE = MOD_PATH + ".proxy.ClientProxy";
 	public static final String SERVERSIDE = MOD_PATH + ".proxy.ServerProxy";
 	
-	public static final int ArmorMaterialDurability = 30;
-	public static final int[] ArmorMaterialReductionAmounts = {2, 6, 5, 2}; 
+	public static final int ArmorMaterialDurability = 35;
+	public static final int[] ArmorMaterialReductionAmounts = {2, 7, 5, 2}; 
 	public static final int ArmorMaterialEnchantability = 9;
 	
 	public static final String[] ItemSteelArmorModel = {
